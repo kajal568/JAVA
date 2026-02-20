72d8d16539860cac81031bdfe56148b383253ea5
kajal568 <K8649497@gmail.com>
2026-02-20 14:06:31 +0000

    datatypes-summmery

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,36 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 2);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log('02' > 1);
+
+
+{
+    //  console.log(null > 0);
+//  console.log(null == 0);
+//  console.log(null >= 0);
+                              // avoid upper and lower type of comparision as they are confusing 
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+}
+
+// === strictly check the comparision 
+
+console.log("2" === '2');
+
+
+
+
+
+
+
+
+
+
+
+
