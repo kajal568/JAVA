b718b39dee483731285b3b7800d9ef6b4464ee40
kajal568 <K8649497@gmail.com>
2026-02-20 14:09:01 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,82 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// '33' => 33
+// '33abd' => NAN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+
+let beeleanisLoggedIn = Boolean(isLoggedIn)
+console.log(beeleanisLoggedIn);
+
+
+
+// 1 => true; 0 => false
+// "" => false
+// "kaju" => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let value = 6
+let negValue = -value
+
+console.log(negValue);
+
+// operations 
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(4/2);
+console.log(2%3);
+
+let str1 = "ka"
+let str2 = "jal"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(3 + "33");
+console.log("1" + 33 + 77);
+console.log(1 + 9 + "8" );
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 66 + 567
+
+let gameCounter = 10000
+gameCounter++;
+console.log(gameCounter);
+
+// prefix postfix difference link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// link to study : https://tc39.es/ecma262/multipage/abstract-operations.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/datatypes-summmery.js
@@ -0,0 +1,39 @@
+// Primitive (call by value)
+
+//  7 types : String , Number , Bigint , Symbol , Null , Undefined , Boolean 
+
+const score = 100 
+const scoreValue = 100.7
+
+const isLoggedIn =  false
+const outsideTemp = null
+let  userEmail;
+
+const  id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+const bigNumber = 6535366879897876756745645345n
+
+
+
+
+// Non Primitive (call by referance)
+
+//Array , Objects , Functions
+
+const heros = ["sktiman", "nagraj", "doga"] // array
+ let myObj = { // object
+
+ name : "Kajal",
+ age : 21,
+
+}
+
+const myFunction = function(){
+    console.log("Hellow world");
+    
+}
+console.log(typeof anotherId);
+// https://www.ecosia.org/search?q=https%3A%2F%2F262.ecma-international.org%2F5.1%2F%23sec-11.4.3&tts=st_asaf_macos
+//https://262.ecma-international.org/5.1/#sec-11.4.3
